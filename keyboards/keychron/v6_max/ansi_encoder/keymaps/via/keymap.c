0e389946a0f6135f4cb297a2a5eca7dad5026850
Wesley Stone <wesley@stonehome.info>
2024-10-31 09:50:02 +1000

    Safe state

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
@@ -36,12 +36,12 @@ enum unicode_names {
 const uint32_t PROGMEM unicode_map[] = {
 	[WOOP] = 0x1F389,
     [TECH] = 0x1F4BB,  // ‽
-    [TICK] = 0xFE0F, // 🐍
+    [TICK] = 0x2713, // 🐍
     [RUDE] = 0x1F595,
 };
 
 enum custom_keycodes {
-	MACRO1,
+	MACRO1 = SAFE_RANGE,
 	MACRO2,
 	MACRO3,
 	MACRO4,
@@ -54,6 +54,9 @@ enum custom_keycodes {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_keychron_common(keycode, record)) {
+        return false;
+    }
 	switch (keycode) {
 		case MACRO1:
 			if (record->event.pressed) {
@@ -85,21 +88,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		    break;
 		case MACRO5:
 		    if (record->event.pressed) {
-		        SEND_STRING(SS_LCTL(X_C) SS_LCTL(SS_LSFT(X_5)) SS_DELAY(1000) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_LCTL(X_V) SS_TAP(X_TAB) "wstone" SS_TAP(X_TAB) "Hippo Enrollment3" SS_TAP(X_ENT));
+		        SEND_STRING(SS_LCTL("c") SS_LCTL(SS_LSFT("5")) SS_DELAY(1000) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_LCTL("v") SS_TAP(X_TAB) "wstone" SS_TAP(X_TAB) "Hippo Enrollment3" SS_TAP(X_ENT));
 		    } else {
 		        // when keycode is released
 		    }
 		    break;
 		case MACRO6:
 		    if (record->event.pressed) {
-		        SEND_STRING(SS_LCTL(SS_LSFT(X_6)));
+		        SEND_STRING(SS_LCTL(SS_LSFT("6")));
 		    } else {
 		        // when keycode is released
 		    }
 		    break;
 		case MACRO7:
 		    if (record->event.pressed) {
-		        SEND_STRING(SS_LCTL(X_C) SS_LCTL(SS_LSFT(X_7)));
+		        SEND_STRING(SS_LCTL("c") SS_LCTL(SS_LSFT("7")));
 		    } else {
 		        // when keycode is released
 		    }
@@ -113,14 +116,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		    break;
 		case MACRO9:
 		    if (record->event.pressed) {
-		        SEND_STRING(SS_LCTL(SS_LSFT(X_9)));
+		        SEND_STRING(SS_LCTL(SS_LSFT("9")));
 		    } else {
 		        // when keycode is released
 		    }
 		    break;
 		case MACRO0:
 		    if (record->event.pressed) {
-		        SEND_STRING(SS(LCTL(SS_LSFT(X_0))));
+		        SEND_STRING(SS_LCTL(SS_LSFT("0")));
 		    } else {
 		        // when keycode is released
 		    }
@@ -133,14 +136,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    KC_SNAP,  KC_SIRI,  RGB_MOD,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    KC_SNAP,  DF(2),  RGB_MOD,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN, MO(MAC_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
     [MAC_FN] = LAYOUT_ansi_109(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,    _______,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,    _______,  DF(2),  RGB_TOG,  _______,  _______,  _______,  _______,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
@@ -154,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
     [WIN_FN] = LAYOUT_ansi_109(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  DF(2),  RGB_TOG,  _______,  _______,  _______,  _______,
         _______,  MACRO1,  MACRO2,  MACRO3,  MACRO4,  MACRO5,  MACRO6,  MACRO7,  MACRO8,  MACRO9, MACRO0,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
@@ -172,9 +175,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #endif // ENCODER_MAP_ENABLE
 
 // clang-format on
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_keychron_common(keycode, record)) {
-        return false;
-    }
-    return true;
-}
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    if (!process_record_keychron_common(keycode, record)) {
+//        return false;
+//    }
+//    return true;
+//}
