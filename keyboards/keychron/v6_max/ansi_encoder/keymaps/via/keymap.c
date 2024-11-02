f2d296a8dd0cc68b988775ccc8b446f61ed1ff23
VulpesVenom <86349784+VulpesVenom@users.noreply.github.com>
2024-11-02 10:59:31 +1000

    Update keymap.c

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
@@ -177,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         LT(3,KC_MS_BTN2),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  LT(WIN_FN,KC_APP), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
     [WIN_FN] = LAYOUT_ansi_109(
         QK_MIDI_ON,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  DF(2),  RGB_TOG,  _______,  _______,  _______,  _______,
         QK_MIDI_NOTE_C_2,  MACRO1,  MACRO2,  MACRO3,  MACRO4,  MACRO5,  MACRO6,  MACRO7,  MACRO8,  MACRO9, MACRO0,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
